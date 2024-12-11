aad9bbc6374534d54b856eea6c5024b85158cf10
stany madanu <stanymadhanu.sm@gmail.com>
2024-12-11 11:37:29 +0530

    404page

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Bot404 from "../assets/images/404.png";
 
 const NotFound = () => {
   return (
-    <div className="card">
-      <div className="card-body text-center">
-        <h3 className="">404 Page Not Found...</h3>
-
-        <span>Take me to </span>
-        <Link className="" to="/">
-          Dashboard
-        </Link>
+    <div className="notfound d-flex align-items-center">
+      <div className="card">
+        <div className="card-body d-flex flex-column align-items-center justify-content-center ">
+          <div>
+            <img src={Bot404} alt="404-image" />
+          </div>
+          <div>
+            <span>Take me to </span>
+            <Link className="" to="/">
+              Dashboard
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
